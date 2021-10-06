978ed2fe09886922d1c946eafdcdf69832e23143
Gennadii Shpreer <61728197+intellegento@users.noreply.github.com>
2021-10-07 00:26:54 +0600

    fix swiper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import "./styles/main.scss";
-import Swiper from './resource/js/script';
+import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
 
 
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 2.5,
+    spaceBetween: 1,
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    navigation: {
+        nextEl: ".slider-header-arrow-next",
+        prevEl: ".slider-header-arrow-prev",
+    },
+});
